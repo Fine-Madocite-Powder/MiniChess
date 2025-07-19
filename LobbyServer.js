7cdfc3f6561f9305875189da4d810bf3d03d8512
SanderOfSand <alex.zeus.gustavsson@gmail.com>
2025-07-19 16:07:27 +0200

    Express to handlebars transition

--- LobbyServer.js
+++ LobbyServer.js
@@ -6,13 +6,22 @@ const port = 3000;
 let app = express();
 let server = http.createServer(app);
 
-
+const {engine} = require('express-handlebars');
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/home.html'));
-
+    res.render("home");
     console.log('A new visitor :D');
 })
+
+app.post('/login', async (req, res) => {
+
+
+})
+
+
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
