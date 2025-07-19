35078caa08ece9a5f7484f325fa06ce933009efa
SanderOfSand <alex.zeus.gustavsson@gmail.com>
2025-07-19 14:29:54 +0200

    Serves home page

--- LobbyServer.js
+++ LobbyServer.js
@@ -1,16 +1,19 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const port = 3000;
 
 let app = express();
 let server = http.createServer(app);
 
+
+
 app.use('/', (req, res) => {
-    res.sendFile('home.html');
+    res.sendFile(path.join(__dirname, '/home.html'));
 
     console.log('A new visitor :D');
 })
 
-server.listen(port, () =>{
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
